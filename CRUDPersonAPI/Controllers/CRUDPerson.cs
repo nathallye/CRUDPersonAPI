44d5720ffed9ce8ca8e98d89675050b294973bfc
Nathallye Bacelar <n.ferreira.tavares@avanade.com>
2022-12-03 18:36:00 -0300

    Add project files.

---
+++ CRUDPersonAPI/Controllers/CRUDPerson.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using CRUDPersonAPI.Repository;
+
+namespace CRUDPersonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRUDPerson : ControllerBase
+    {
+        [HttpGet]
+        [Route("Person")]
+        public IActionResult Get()
+        {
+            try
+            {
+                return base.Ok(new PersonRepository().GetAll());
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
