de10cfc04277763cdb3470bc98b2f276c58d0d20
Nathallye Bacelar <nathallyet@gmail.com>
2022-12-19 15:33:27 -0300

    update

--- Data/Dto/PersonUpdateDto.cs
+++ Data/Dto/PersonUpdateDto.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? LastName { get; set; }
-        public string? CPF { get; set; } = null!;
-        public string? Email { get; set; } = null!;
+        public string? CPF { get; set; } 
+        public string? Email { get; set; } 
         public string? Address { get; set; }
     }
 }
